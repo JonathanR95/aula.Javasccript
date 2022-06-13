e54f55de4f52bd794cc1e00a1c18b2a6fa884952
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-06-13 09:26:43 -0300

    calculando o primeiro IMC

---
+++ introducao-javascript-inicial/introducao-javascript/js/principal.js
@@ -0,0 +1,22 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector('#primeiro-paciente');
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura= paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var tdImc = paciente. querySelector('.info-imc');
+
+var imcDaLinha = tdImc.textContent; 
+
+var imc = peso / (altura * altura);
+console.log(imc);
+
+tdImc.textContent = imc;
+
+
+
