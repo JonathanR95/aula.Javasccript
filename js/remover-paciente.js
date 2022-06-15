66a480181218e4d4be9abfa57218510560e61ce1
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-06-15 08:00:29 -0300

    adicionando evento para excluir paciente

---
+++ js/remover-paciente.js
@@ -0,0 +1,8 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){  
+    event.target.parentNode.remove();
+});
+
